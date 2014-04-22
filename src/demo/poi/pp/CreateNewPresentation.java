f3652f940d5e129d2d5a03677886fa32cbd75bcf
Chris Wong <wc38.wong@gmail.com>
2014-04-22 17:07:40 +0800

    How to create a new power point presentation

---
+++ src/demo/poi/pp/CreateNewPresentation.java
@@ -0,0 +1,64 @@
+package demo.poi.pp;
+
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+import org.apache.poi.xslf.usermodel.SlideLayout;
+import org.apache.poi.xslf.usermodel.XMLSlideShow;
+import org.apache.poi.xslf.usermodel.XSLFSlide;
+import org.apache.poi.xslf.usermodel.XSLFSlideLayout;
+import org.apache.poi.xslf.usermodel.XSLFSlideMaster;
+import org.apache.poi.xslf.usermodel.XSLFTextShape;
+
+/**
+ * @author Chris Wong
+ * <p>
+ * <a href="http://poi.apache.org/slideshow/xslf-cookbook.html">CreateNewPresentation</a>
+ * </p>
+ */
+public class CreateNewPresentation {
+	public static void main(String[] args) {
+		XMLSlideShow slideShow = new XMLSlideShow();
+		
+		// create a blank slide
+		XSLFSlide slide = slideShow.createSlide();
+		
+		// there can be multiple masters each referencing a number of layouts
+	    // for demonstration purposes we use the first (default) slide master
+	    XSLFSlideMaster defaultMaster = slideShow.getSlideMasters()[0];
+
+	    // title slide
+	    XSLFSlideLayout titleLayout = defaultMaster.getLayout(SlideLayout.TITLE);
+	    // fill the placeholders
+	    XSLFSlide slide1 = slideShow.createSlide(titleLayout);
+	    XSLFTextShape title1 = slide1.getPlaceholder(0);
+	    title1.setText("First Title");
+
+	    // title and content
+	    XSLFSlideLayout titleBodyLayout = defaultMaster.getLayout(SlideLayout.TITLE_AND_CONTENT);
+	    XSLFSlide slide2 = slideShow.createSlide(titleBodyLayout);
+
+	    XSLFTextShape title2 = slide2.getPlaceholder(0);
+	    title2.setText("Second Title");
+
+	    XSLFTextShape body2 = slide2.getPlaceholder(1);
+	    body2.clearText(); // unset any existing text
+	    body2.addNewTextParagraph().addNewTextRun().setText("First paragraph");
+	    body2.addNewTextParagraph().addNewTextRun().setText("Second paragraph");
+	    body2.addNewTextParagraph().addNewTextRun().setText("Third paragraph");
+		FileOutputStream out;
+		try {
+			out = new FileOutputStream("slideshow.ppt");
+			slideShow.write(out);
+			out.close();
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
+}
